3f38319f383c78f365eef22f1c720009c6d13619
iranianboy <alonereza18@gmail.com>
2016-09-20 13:24:39 +0430

    Update bot.lua

--- plugins/bot.lua
+++ plugins/bot.lua
@@ -6,9 +6,9 @@
 --                                              --
 --------------------------------------------------
 --                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
---              @h3iran :D                      --
+--                                              --
+--     Support: @cyruspedia,                    --
+--                                              --
 --------------------------------------------------
 local function is_channel_disabled( receiver )
 	if not _config.disabled_channels then
